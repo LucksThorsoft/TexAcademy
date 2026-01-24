5b6c51da7052acacc6b7df0135e377af19490dc7
Armando Larios <armandolariosoft@gmail.com>
2026-01-24 17:31:43 -0600

    Implementacion de Inicio de sesion y logout

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('dashboard', views.dashboard),
     path('grupos-alumnos', views.gruposAlumnos),
     path('actividades', views.actividades),
--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.hashers import check_password
+from .forms import *
+from .models import *
 
 # Create your views here.
 
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "home.html")
 
 def dashboard(request):
     return render(request, "dashboard.html")
@@ -26,3 +26,41 @@ def alertas(request):
     return render(request, "alertas.html")
 
 
+def login_view(request):
+    # Si el usuario ya está logueado, lo mandamos directo al dashboard
+    if request.session.get('usuario_id'):
+        return redirect('/dashboard')
+
+    if request.method == 'POST':
+        # Capturamos los datos directamente de los inputs HTML por su 'name'
+        correo = request.POST.get('correo')
+        password = request.POST.get('password')
+
+        if not correo or not password:
+            messages.error(request, "Por favor completa todos los campos")
+        else:
+            try:
+                usuario = Usuario.objects.get(correo=correo)
+                
+                # Verificar contraseña (asegúrate de que en la BD estén hasheadas)
+                # Si en tu BD las tienes como texto plano (mal práctica pero posible),
+                # cambia esta línea por: if password == usuario.password:
+                # if check_password(password, usuario.password):
+                if password == usuario.password:
+                    # --- ÉXITO ---
+                    request.session['usuario_id'] = usuario.id
+                    request.session['usuario_nombre'] = usuario.nombre
+                    # Opcional: Guardar rol si lo necesitas
+                    # request.session['usuario_rol'] = ... 
+                    return redirect('/dashboard')
+                else:
+                    messages.error(request, 'Contraseña incorrecta.')
+            
+            except Usuario.DoesNotExist:
+                messages.error(request, 'El correo no está registrado.')
+
+    return render(request, "login.html")
+
+def logout_view(request):
+    request.session.flush() # Borra toda la sesión
+    return redirect('login')
