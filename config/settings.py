0e3b3d4c9405675277e5860ee46df6ba8f4e68fa
Melus <a20230146@utem.edu.mx>
2026-03-17 15:34:16 -0600

    Implementacion de QR

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tm(3e8-*@y-gb^8jwybx01csa&p@)q+n@dqjgf*mc0w^ac9u4h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.11', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.3.74', 'localhost', '127.0.0.1']
 
 
 # Application definition
